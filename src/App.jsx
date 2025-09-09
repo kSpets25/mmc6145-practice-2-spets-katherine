e869dc835950a95c47c4cbfcc8021203ad3fbcaa
kSpets25 <142757766+kSpets25@users.noreply.github.com>
2025-09-08 23:29:28 -0500

    update app
    added calculator function. added css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // App.js
-import React, { useState } from 'react';
+import { useState } from 'react';
 import './App.css'; // For styling
 
 //assignment limitations
@@ -8,11 +8,40 @@ import './App.css'; // For styling
 
 
 export default function App(){
+  const [number1, setNumber1] = useState('');
+  const [number2, setNumber2] = useState('');
+  const [total, setTotal] = useState(0);
+
+ 
+  function calculateTotal() {
+    setTotal (number1 + number2)
+    
+  }
   return (
-  <>
-  </>
+    <div>
+    <h1>Adding Calculator</h1>
+    <input
+      placeholder="First Number"
+      type="number"
+      value={number1}
+      onChange={(e) => 
+      setNumber1(+e.target.value)}
+      />
+      <span> + </span>
+      <input
+      placeholder="Second Number"
+      type="number"
+      value={number2}
+      onChange={(e) => 
+      setNumber2(+e.target.value)} 
+      />
+      <button onClick={calculateTotal}>Add Two Numbers</button>
+      <p>Total: {total || ""}</p>
+    </div>
 
   )
 }
+
+
 
 
