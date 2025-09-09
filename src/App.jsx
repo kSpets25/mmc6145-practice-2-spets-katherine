f7e120d9faae2741b16167debeddfbf23aabdd66
kSpets25 <142757766+kSpets25@users.noreply.github.com>
2025-09-09 11:28:59 -0500

    update app
    added number value function and css

--- src/App.jsx
+++ src/App.jsx
@@ -2,41 +2,39 @@
 import { useState } from 'react';
 import './App.css'; // For styling
 
-//assignment limitations
-//calculator only accept two numbers per operation.
-//calculator will only have one operation available.
-
 
 export default function App(){
   const [number1, setNumber1] = useState('');
   const [number2, setNumber2] = useState('');
   const [total, setTotal] = useState(0);
 
- 
   function calculateTotal() {
     setTotal (number1 + number2)
-    
+
+  function MyComponent({ stringValue }) {
+      const numberValue = Number(stringValue); // Converts "05" to 5, "007" to 7
+      return <div>{numberValue}</div>;
+      }
+ 
   }
   return (
     <div>
-    <h1>Adding Calculator</h1>
-    <input
-      placeholder="First Number"
-      type="number"
-      value={number1}
-      onChange={(e) => 
-      setNumber1(+e.target.value)}
-      />
-      <span> + </span>
-      <input
-      placeholder="Second Number"
-      type="number"
-      value={number2}
-      onChange={(e) => 
-      setNumber2(+e.target.value)} 
-      />
-      <button onClick={calculateTotal}>Add Two Numbers</button>
-      <p>Total: {total || ""}</p>
+        <h1>Adding Calculator</h1>
+        <input
+          placeholder="Number" //using in place of label 
+          type="number"
+          numberValue={number1}
+          onChange={(e) => setNumber1(+e.target.value)}
+          />
+          <span> + </span>
+          <input
+          placeholder="Number" 
+          type="number"
+          numberValue={number2}
+          onChange={(e) => setNumber2(+e.target.value)} 
+          />
+          <button onClick={calculateTotal}>Add Two Numbers</button>
+          <p>Total: {total || ""}</p>
     </div>
 
   )
